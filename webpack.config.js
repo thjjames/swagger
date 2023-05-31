6c18c7aa1f48f1183a6bce7b7b878a72e0bb150e
James <tianhaojun@bilibili.com>
2023-05-31 18:38:14 +0800

    build: support es6-module dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,61 +4,71 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isDev = argv.mode === 'development';
-  return {
-    entry: {
-      main: isDev ? './test/index.js' : './src/index.js'
-    },
-    output: {
-      filename: 'swagger.min.js',
-      library: {
-        name: 'SwaggerApi',
-        type: 'umd',
-        export: 'default',
-        umdNamedDefine: true,
+
+  let types = ['module', 'umd'];
+  if (isDev) types = types.slice(1);
+
+  return types.map(type => {
+    const isModule = type === 'module';
+    return {
+      entry: {
+        main: isDev ? './test/index.js' : './src/index.js'
       },
-      globalObject: 'this'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
+      output: {
+        filename: `swagger.min.${isModule ? 'mjs' : 'js'}`,
+        library: {
+          name: isModule ? void 0 : 'SwaggerApi',
+          type,
+          export: 'default',
+          umdNamedDefine: true,
         },
-      ]
-    },
-    plugins: (isDev ? [
-      new HtmlWebpackPlugin({ template: path.join(__dirname, 'test/index.html') })
-    ] : []).concat([
-      new webpack.DefinePlugin({
-        noop: () => void 0
-      })
-    ]),
-    resolve: {
-      extensions: ['.js']
-    },
-    devtool: isDev && 'eval-source-map',
-    externals: isDev ? {
-      'vue': 'Vue',
-      'element-ui': 'ELEMENT'
-    } : {},
-    devServer: {
-      allowedHosts: 'auto',
-      historyApiFallback: true,
-      host: 'local-ip',
-      port: 'auto',
-      hot: true,
-      open: true,
-      proxy: {
-        '/api': {
-          target: 'https://getman.cn/api',
-          pathRewrite: {'^/api': ''},
-          secure: false, // 如果是https接口，需要配置这个参数
-          changeOrigin: true
+        globalObject: 'this'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+        ]
+      },
+      plugins: (isDev ? [
+        new HtmlWebpackPlugin({ template: path.join(__dirname, 'test/index.html') })
+      ] : []).concat([
+        new webpack.DefinePlugin({
+          noop: () => void 0
+        })
+      ]),
+      resolve: {
+        extensions: ['.js']
+      },
+      devtool: isDev && 'eval-source-map',
+      externals: isDev ? {
+        'vue': 'Vue',
+        'element-ui': 'ELEMENT'
+      } : {},
+      devServer: {
+        allowedHosts: 'auto',
+        historyApiFallback: true,
+        host: 'local-ip',
+        port: 'auto',
+        hot: true,
+        open: true,
+        proxy: {
+          '/api': {
+            target: 'https://getman.cn/api',
+            pathRewrite: {'^/api': ''},
+            secure: false, // 如果是https接口，需要配置这个参数
+            changeOrigin: true
+          }
         }
+      },
+      experiments: {
+        outputModule: isModule
       }
-    }
-  };
+    };
+  });
 };
